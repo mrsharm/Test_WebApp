f8bd5a660674b7a9ba5f0cbd910d965ef336ee78
Mukund Raghav Sharma <musharm@microsoft.com>
2025-04-17 19:26:59 -0700

    Added details

---
+++ Controllers/AppController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_AppService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("appinvoke")]
+        public ActionResult<string> appinvoke()
+        {
+            Subscriber.CreatePublishers();
+            return "Created multiple subscribers to the publisher!";
+        }
+
+        [HttpGet]
+        [Route("sayhello")]
+        public ActionResult<string> sayhello()
+        {
+            return "Hello World!";
+        }
+    }
+}
---
+++ PublisherSubscriber.cs
@@ -0,0 +1,39 @@
+﻿namespace WebApp_AppService
+{
+    public class Publisher
+    {
+        public event EventHandler SomeEvent;
+
+        public void RaiseEvent()
+        {
+            SomeEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Subscriber
+    {
+        private readonly byte[] _largeData = new byte[1024 * 1024]; // 1MB of data
+
+        public void Subscribe(Publisher publisher)
+        {
+            publisher.SomeEvent += OnEvent; // Never unsubscribed
+        }
+
+        private void OnEvent(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Event received, data size: {_largeData.Length}");
+        }
+
+        public static Publisher CreatePublishers()
+        {
+            var publisher = new Publisher();
+            for (int i = 0; i < 2_000; i++)
+            {
+                var subscriber = new Subscriber();
+                subscriber.Subscribe(publisher);
+            }
+
+            return publisher;
+        }
+    }
+}
