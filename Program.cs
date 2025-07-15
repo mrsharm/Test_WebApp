06333ece12c8ff0db946f4746cbca400b109f428
Mukund Raghav Sharma <musharm@microsoft.com>
2025-07-15 11:57:01 -0700

    Added Config issue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Azure.Storage.Blobs;
 
 namespace WebApp_AppService
 {
@@ -6,8 +7,6 @@ namespace WebApp_AppService
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,7 +26,22 @@ namespace WebApp_AppService
             app.UseAuthorization();
             app.MapControllers();
 
-            Subscriber.CreatePublishers();
+            var connectionString = app.Configuration.GetConnectionString("StorageAccount");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Storage account connection string is not configured.");
+            }
+
+            try
+            {
+                var blobServiceClient = new BlobServiceClient(connectionString);
+                var accountInfo = blobServiceClient.GetAccountInfo();
+                Console.WriteLine("Successfully connected to storage account.");
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Failed to connect to storage account: {ex.Message}", ex);
+            }
 
             app.Run();
         }
